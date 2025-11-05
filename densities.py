390e0e511ec993c403bc7248feeefcd1d35e39cf
SpookedBones6 <nielshouben6@gmail.com>
2025-11-05 15:55:53 +0100

    Calculates and plots density fits with parameters and errors as output

--- densities.py
+++ densities.py
@@ -10,3 +10,8 @@ def plummer(r_kpc, mass_sol, param_kpc):
     constant = (3*mass_sol)/(4*np.pi*(param_kpc**3))
     density = constant*(1 + (r_kpc**2)/(param_kpc**2))**(-5/2)
     return density
+
+def doublepowerlaw(r_kpc, ampl_solm, a_kpc, alpha, beta):
+    constant = ampl_solm/(4*np.pi*(a_kpc**3))
+    density = constant/(((r_kpc/a_kpc)**alpha)*(1.0+(r_kpc/a_kpc)**(beta-alpha)))
+    return density
